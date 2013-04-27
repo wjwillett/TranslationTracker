1214cb312c2901ed0b10eae8babfab902a2f423a
Matthew Mauriello <mattm@cs.umd.edu>
2013-04-27 17:16:53 -0400

    including textHighlights and Styles
    This is the basic functions file I have completed thus far, including
    notations on the constructor and how these functions should be evoked.
    Additionally, added basic starter stylesheet for the project.

---
+++ TranslationTrackerChromeExtension/textHighlights.js
@@ -0,0 +1,59 @@
+/**
+* jQuery plugin to replace text strings
+* Taken from @link http://net.tutsplus.com/tutorials/javascript-ajax/spotlight-jquery-replacetext/
+*/
+
+$.fn.replaceText = function( search, replace, text_only ) {
+return this.each(function(){
+        var node = this.firstChild,
+        val, new_val, remove = [];
+        if ( node ) {
+            do {
+              if ( node.nodeType === 3 ) {
+                val = node.nodeValue;
+                new_val = val.replace( search, replace );
+                if ( new_val !== val ) {
+                  if ( !text_only && /</.test( new_val ) ) {
+                    $(node).before( new_val );
+                    remove.push( node );
+                  } else {
+                    node.nodeValue = new_val;
+                  }
+                }
+              }
+            } while ( node = node.nextSibling );
+        }
+        remove.length && $(remove).remove();
+    });
+};
+
+/**
+* jQuery wrapper to process standard tags in the target document and assign span styles.
+* Expects JSON properly formatted
+*/
+
+processHighlights = function(Input) {
+	for(i = 0; i < Input.length; i++){ 
+		$('body').replaceText(Input[i].Word, '<span class="highlight'+Input[i].Color+'">$1</span>');
+		$('div').replaceText(Input[i].Word, '<span class="highlight'+Input[i].Color+'">$1</span>');
+		$('p').replaceText(Input[i].Word, '<span class="highlight'+Input[i].Color+'">$1</span>');
+	}
+}
+
+/* //Example Constructor: 
+ * 
+ * NOTE: Dynamic appending of REQUIRED style sheet and JSON format of Words variable.
+ *
+ * var Words = 
+ * [
+ *	{ "Word": /(sample)/gi , "Color": 1},
+ *	{ "Word": /(text)/gi   , "Color": 2},
+ *	{ "Word": /(lorem)/gi  , "Color": 3}
+ * ]
+ *
+ * //Call function on document ready
+ * $(document).ready(function() {
+ *  $('head').append('<link rel="stylesheet" type="text/css" href="color_level_styles.css">');	 
+ * 	processHighlights(Words);
+ * });
+ */
